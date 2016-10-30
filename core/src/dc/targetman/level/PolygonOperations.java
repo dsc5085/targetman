9df44d2991a961bedcf545c71003338c00d78d2a
dave1 <dave1@LAPTOP-4I0UEUC1.home>
2016-10-30 19:51:15 -0400

    Polygon union cleanup

--- core/src/dc/targetman/level/PolygonOperations.java
+++ core/src/dc/targetman/level/PolygonOperations.java
@@ -8,49 +8,51 @@ import com.badlogic.gdx.math.Vector2;
 public final class PolygonOperations {
 
 	public static void union(final List<Vector2> parentVertices, final List<Vector2> newVertices) {
-		for (int i = 0; i < parentVertices.size(); i++) {
-			Vector2 parentVertex = parentVertices.get(i);
-			int newVertexIndex = newVertices.indexOf(parentVertex);
+		for (int parentIndex = 0; parentIndex < parentVertices.size() - 1; parentIndex++) {
+			int newVertexIndex = newVertices.indexOf(parentVertices.get(parentIndex));
 			if (newVertexIndex >= 0) {
-				List<Vector2> addedVertexChain = getVertexChain(parentVertices, newVertices, newVertexIndex + 1);
-				parentVertices.addAll(i, addedVertexChain);
-				i += addedVertexChain.size();
-				removeVertexChain(parentVertices, newVertices, i);
-				i--;
+				List<Vector2> vertexChainToAdd = getVertexChain(parentVertices, newVertices, newVertexIndex + 1);
+				parentVertices.addAll(parentIndex, vertexChainToAdd);
+				parentIndex += vertexChainToAdd.size();
+				removeVertexChain(parentVertices, newVertices, parentIndex);
+				parentIndex--;
 			}
 		}
 	}
 
-	private static void removeVertexChain(final List<Vector2> parentVertices, final List<Vector2> newVertices,
-			final int startIndex) {
-		for (int i = startIndex; i < parentVertices.size(); i++) {
-			if (newVertices.contains(parentVertices.get(i))) {
-				parentVertices.remove(i);
-				i--;
-			} else {
-				break;
-			}
-		}
-	}
-
-	private static List<Vector2> getVertexChain(final List<Vector2> parentVertices, final List<Vector2> newVertices,
-			final int startIndex) {
+	private static List<Vector2> getVertexChain(final List<Vector2> parentVertices,
+			final List<Vector2> newVertices, final int start) {
 		List<Vector2> vertexChain = new ArrayList<Vector2>();
-		int i = startIndex;
+		int newVertexIndex = start;
 		boolean adding = true;
 		while (adding) {
-			if (i >= newVertices.size()) {
-				i = 0;
-			}
-			Vector2 newVertex = newVertices.get(i);
+			newVertexIndex = wrapIndex(newVertexIndex, newVertices);
+			Vector2 newVertex = newVertices.get(newVertexIndex);
 			if (parentVertices.contains(newVertex) || vertexChain.contains(newVertex)) {
 				adding = false;
 			} else {
 				vertexChain.add(newVertex);
-				i++;
+				newVertexIndex++;
 			}
 		}
 		return vertexChain;
+	}
+
+	private static void removeVertexChain(final List<Vector2> parentVertices, final List<Vector2> newVertices,
+			final int start) {
+		while (start < parentVertices.size() && newVertices.contains(parentVertices.get(start))) {
+			parentVertices.remove(start);
+		}
+	}
+
+	private static int wrapIndex(final int index, final List<Vector2> vertices) {
+		int wrappedIndex = index;
+		if (index < 0) {
+			wrappedIndex = vertices.size() - 1;
+		} else if (index >= vertices.size()) {
+			wrappedIndex = 0;
+		}
+		return wrappedIndex;
 	}
 
 }
