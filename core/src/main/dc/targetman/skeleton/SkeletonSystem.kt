62a8d484d506b8b50ab48d906347ffdc5e3b7720
dsc5085 <dchen5085@gmail.com>
2016-12-26 20:07:01 -0500

    More fixes to bone scaling

--- core/src/main/dc/targetman/skeleton/SkeletonSystem.kt
+++ core/src/main/dc/targetman/skeleton/SkeletonSystem.kt
@@ -1,5 +1,6 @@
 package dc.targetman.skeleton
 
+import com.badlogic.gdx.math.Matrix3
 import com.badlogic.gdx.math.Vector2
 import com.esotericsoftware.spine.Skeleton
 import com.esotericsoftware.spine.attachments.RegionAttachment
@@ -49,24 +50,28 @@ class SkeletonSystem(entityManager: EntityManager) : EntitySystem(entityManager)
     private fun updateLimbTransform(limbName: String, limb: Entity, skeleton: Skeleton) {
         // TODO: cleanup
         val bone = skeleton.bones.single { it.data.name == limbName }
-        val flipScale = VectorUtils.sign(Vector2(skeleton.rootBone.scaleX, skeleton.rootBone.scaleY))
-        val scale = Vector2(bone.worldScaleX, bone.worldScaleY).scl(flipScale)
         val transform = limb[TransformPart::class].transform
         val origin = transform.size.scl(0.5f)
         val newWorld = Vector2(bone.worldX, bone.worldY)
         transform.rotation = bone.worldRotationX
         val attachment = skeleton.slots.filter { it.bone.data.name == limbName }.map { it.attachment }
                 .filterIsInstance<RegionAttachment>().firstOrNull()
-        var attachmentScaledRotation = 0f
+        val flipScale = VectorUtils.sign(Vector2(skeleton.rootBone.scaleX, skeleton.rootBone.scaleY))
+        val boneScale = Vector2(bone.worldScaleX, bone.worldScaleY).scl(flipScale)
         if (attachment is RegionAttachment) {
-            attachmentScaledRotation = getScaledRotation(attachment.rotation, scale)
-            transform.rotation += attachmentScaledRotation
-            val offsetRotation = getScaledRotation(bone.worldRotationX, scale)
-            val localOffset = Vector2(attachment.x, attachment.y).rotate(offsetRotation).scl(scale)
+            transform.rotation += getScaledRotation(attachment.rotation, boneScale)
+            val offsetRotation = getScaledRotation(bone.worldRotationX, boneScale)
+            transform.scale = calculateTransformScale(boneScale, flipScale, attachment.rotation)
+            val localOffset = Vector2(attachment.x, attachment.y).rotate(offsetRotation).scl(transform.scale)
             newWorld.add(localOffset)
         }
-        transform.scale = VectorUtils.abs(Vector2(bone.worldScaleX, bone.worldScaleY).rotate(attachmentScaledRotation)).scl(flipScale)
         transform.setWorld(origin, newWorld)
+    }
+
+    private fun calculateTransformScale(boneScale: Vector2, flipScale: Vector2, attachmentRotation: Float): Vector2 {
+        val matrix = Matrix3().rotate(attachmentRotation).scale(boneScale).rotate(-attachmentRotation)
+        val scale = matrix.getScale(Vector2())
+        return VectorUtils.abs(scale).scl(flipScale)
     }
 
     private fun getScaledRotation(degrees: Float, scale: Vector2): Float {
