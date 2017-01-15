d84e5821b72cdd3bcfd9037cb6f7c6add16a41a8
David Chen <dchen5085@gmail.com>
2017-01-14 21:05:05 -0500

    Updated man texture to be vector based

--- core/src/main/dc/targetman/level/LevelController.kt
+++ core/src/main/dc/targetman/level/LevelController.kt
@@ -105,7 +105,7 @@ class LevelController(
 		mapRenderer.setView(camera)
 		mapRenderer.render()
 		renderEntities()
-		renderBox2D()
+//		renderBox2D()
 	}
 
 	private fun createEntityManager(): EntityManager {
