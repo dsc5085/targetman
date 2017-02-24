a72e79002a88c7916d5aba4d949be59d73aae42c
David Chen <dchen5085@gmail.com>
2017-02-23 22:30:58 -0500

    Fixed gun aiming TODO

--- core/src/main/dc/targetman/mechanics/weapon/AimOnAnimationApplied.kt
+++ core/src/main/dc/targetman/mechanics/weapon/AimOnAnimationApplied.kt
@@ -1,20 +1,20 @@
 package dc.targetman.mechanics.weapon
 
 import dc.targetman.epf.parts.FiringPart
+import dc.targetman.epf.parts.SkeletonPart
 import dc.targetman.skeleton.AnimationAppliedEvent
+import dclib.geometry.VectorUtils
 
 class AimOnAnimationApplied : (AnimationAppliedEvent) -> Unit {
     override fun invoke(event: AnimationAppliedEvent) {
         val firingPart = event.entity.tryGet(FiringPart::class)
         if (firingPart != null) {
-            // TODO:
-//            val skeletonPart = event.entity[SkeletonPart::class]
-//            val muzzle = skeletonPart[firingPart.muzzleName]
-//            val muzzleBoneRotation = VectorUtils.getScaledRotation(muzzle.bone.worldRotationX, muzzle.scale)
-//            val rotationOffset = firingPart.aimRotation - muzzleBoneRotation
-//            val rotatorBone = skeletonPart[firingPart.rotatorName].bone
-//            rotatorBone.rotation += rotationOffset
-//            skeletonPart.skeleton.updateWorldTransform()
+            val skeletonPart = event.entity[SkeletonPart::class]
+            val rotator = skeletonPart[firingPart.rotatorName]
+            val muzzleBoneRotation = VectorUtils.getScaledRotation(rotator.bone.worldRotationX, rotator.scale)
+            val rotationOffset = firingPart.aimRotation - muzzleBoneRotation
+            rotator.bone.rotation += rotationOffset
+            skeletonPart.skeleton.updateWorldTransform()
         }
     }
 }
