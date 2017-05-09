9677dc87141c2b0258935b0761e4ac4618595f20
David Chen <dchen5085@gmail.com>
2017-05-08 21:23:08 -0400

    Create console screen class

--- core/src/main/dc/targetman/screens/ConsoleScreen.kt
+++ core/src/main/dc/targetman/screens/ConsoleScreen.kt
@@ -1,3 +1,65 @@
 package dc.targetman.screens
 
-class ConsoleScreen
+import com.badlogic.gdx.Screen
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.ui.TextField
+import com.badlogic.gdx.utils.viewport.ScreenViewport
+import dc.targetman.command.CommandProcessor
+import dclib.ui.StageUtils
+import dclib.ui.UiPack
+
+class ConsoleScreen(private val commandProcessor: CommandProcessor, private val uiPack: UiPack) : Screen {
+    private val stage = createStage()
+
+    override fun hide() {
+    }
+
+    override fun show() {
+    }
+
+    override fun render(delta: Float) {
+        stage.act(delta)
+        draw()
+    }
+
+    override fun pause() {
+    }
+
+    override fun resume() {
+    }
+
+    override fun resize(width: Int, height: Int) {
+        StageUtils.resize(stage, width, height)
+    }
+
+    override fun dispose() {
+        stage.dispose()
+    }
+
+    private fun createStage(): Stage {
+        val stage = Stage(ScreenViewport())
+        val mainTable = createMainTable()
+        stage.addActor(mainTable)
+        return stage
+    }
+
+    private fun createMainTable(): Table {
+        val mainTable = uiPack.table()
+        val textField = uiPack.textField()
+        textField.setTextFieldListener(this::handleKeyTyped)
+        mainTable.add(textField)
+        return mainTable
+    }
+
+    private fun handleKeyTyped(textField: TextField, c: Char) {
+        if (c == '\n') {
+            commandProcessor.execute(textField.text)
+            textField.clear()
+        }
+    }
+
+    private fun draw() {
+        stage.draw()
+    }
+}
