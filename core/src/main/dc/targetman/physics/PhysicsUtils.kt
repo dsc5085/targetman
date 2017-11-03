2fcbdca23d955abe6bb2fce55a3c9e88ab48028d
David Chen <dchen5085@gmail.com>
2017-11-02 20:35:13 -0400

    Apply force to individual limbs

--- core/src/main/dc/targetman/physics/PhysicsUtils.kt
+++ core/src/main/dc/targetman/physics/PhysicsUtils.kt
@@ -2,9 +2,11 @@ package dc.targetman.physics
 
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.World
+import dc.targetman.epf.parts.SkeletonPart
 import dc.targetman.skeleton.LimbUtils
 import dclib.epf.Entity
 import dclib.epf.parts.TransformPart
+import dclib.physics.Transform
 
 object PhysicsUtils {
     fun createWorld(): World {
@@ -12,9 +14,13 @@ object PhysicsUtils {
     }
 
     fun applyForce(entities: Collection<Entity>, target: Entity, force: Vector2) {
+        val targetTransform: Transform
         val targetContainer = LimbUtils.findContainer(entities, target)
-        if (targetContainer != null) {
-            targetContainer[TransformPart::class].transform.applyImpulse(force)
+        if (targetContainer != null && targetContainer[SkeletonPart::class].isEnabled) {
+            targetTransform = targetContainer[TransformPart::class].transform
+        } else {
+            targetTransform = target[TransformPart::class].transform
         }
+        targetTransform.applyImpulse(force)
     }
 }
