6edf99847742c07afa459ac0cd9a2302d2be8665
dsc5085 <dchen5085@gmail.com>
2017-12-23 13:54:56 -0500

    Fixed missing ActionsPart bug

--- core/src/main/dc/targetman/mechanics/InventoryActions.kt
+++ core/src/main/dc/targetman/mechanics/InventoryActions.kt
@@ -32,9 +32,11 @@ class InventoryActions(factoryTools: FactoryTools) {
             val sourceEntity = LimbUtils.findContainer(entityManager.getAll(), collidedEvent.source)
             if (sourceEntity != null) {
                 val inventoryPart = sourceEntity.tryGet(InventoryPart::class)
-                val pickup = sourceEntity[ActionsPart::class][ActionKey.PICKUP].doing
-                if (inventoryPart != null && pickup && inventoryPart.pickupTimer.check()) {
-                    pickup(inventoryPart, sourceEntity[SkeletonPart::class], pickupEntity)
+                if (inventoryPart != null) {
+                    val pickup = sourceEntity[ActionsPart::class][ActionKey.PICKUP].doing
+                    if (pickup && inventoryPart.pickupTimer.check()) {
+                        pickup(inventoryPart, sourceEntity[SkeletonPart::class], pickupEntity)
+                    }
                 }
             }
         }
